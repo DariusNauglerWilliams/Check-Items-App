8473f845b6ee0943db29ab20297b05eb37b7b601
Darius Naugler-Williams <djnaugler@gmail.com>
2026-06-22 12:41:09 -0300

    with '#' will be ignored, and an empty message aborts the commit.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,31 @@
-import * as React from "react"
+import * as React from "react";
+import reactLogo from "../assets/react.svg";
+import './Navbar.css'
 
 function Navbar() {
 
   return(
 <>
 
-<nav>
+<nav className="Navbar">
+    <div>
+<p className="TextLogo">InOut<img src={reactLogo} alt="InPeaceLogo" className="ImageLogo"></img> </p> 
+</div>
+<div className="navbar-search">
+    Search
+  </div>
 
-
-    
+<div>
+<ul className="NavbarLinks">
+<li>Home</li>
+<li>Scan</li>
+<li>History</li>
+<li>Compare</li>
+<li>About</li>
+<li>Details</li>
+<li>Contact</li>
+</ul>
+</div>
 </nav>
 
 </>
